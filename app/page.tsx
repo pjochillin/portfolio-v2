f985ce32268c1aa9353f4743ad7a3052d922dc66
Joshua Ochalek <pjochillin@gmail.com>
2025-04-03 20:34:51 -0400

    Add head meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Intro from "../components/intro"
 import Jobs from "../components/jobs"
 import Cello from "../components/cello"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Josh Ochalek's Portfolio</title>
+        <meta 
+          name="description"
+          content="Hello! I'm Josh, a student at Cornell University. Check out my portfolio!"
+        />
+      </Head>
       <Intro />
       <Jobs />
       <Cello />
