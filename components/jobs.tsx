8d1c671f3af90c55b25a3f643d221e0609b5b901
Joshua Ochalek <pjochillin@gmail.com>
2025-04-03 20:32:26 -0400

    Final fix for temp dark mode

--- components/jobs.tsx
+++ components/jobs.tsx
@@ -11,7 +11,7 @@ export default function jobs() {
             <div className="absolute left-12 top-8 bottom-8 hidden min-[1200px]:block w-2 bg-black rounded-full"></div>
             <div className="flex flex-col space-y-18 my-18 min-[500px]:my-24">
                 <h1 className="text-3xl min-[450px]:text-4xl min-[750px]:text-5xl dark:text-black text-black font-bold self-center">Highlighted Experience</h1>
-                <div className="grid grid-cols-1 min-[500px]:grid-cols-2 gap-y-8 gap-x-2 min-[550px]:gap-y-12 min-[550px]:gap-x-12 min-[850px]:flex min-[850px]:justify-around min-[850px]:space-x-2 min-[1000px]:space-x-12 min-[1250px]:space-x-24">
+                <div className="grid grid-cols-1 min-[500px]:grid-cols-2 gap-y-8 gap-x-2 min-[550px]:gap-y-12 min-[550px]:gap-x-12 min-[850px]:flex min-[850px]:justify-around min-[850px]:space-x-2 min-[1000px]:space-x-12 min-[1250px]:space-x-24 dark:text-black text-black">
                     <div className="flex flex-col items-center space-y-0.5">
                         <Image
                             src="/code.png"
