ac72a8f683ceeaef193f89be61e2eeb0c94b384c
Joshua Ochalek <pjochillin@gmail.com>
2025-04-03 20:27:10 -0400

    Try and temporarily combat dark mode

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: false,
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      // etc.
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+}
