19198beb10a504ad40b0db824951448667ce89dc
Joshua Ochalek <pjochillin@gmail.com>
2025-04-03 20:35:51 -0400

    Align footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function footer() {
                 <div className="w-[95%] mt-48 ml-1 bg-white h-0.5 rounded-full"></div>
             </div>
             <div className="pb-8 pt-8 flex justify-between">
-                <h3 className="text-white ml-12">© Josh Ochalek 2025</h3>
+                <h3 className="text-white ml-6 min-[500px]:ml-12">© Josh Ochalek 2025</h3>
             </div>
         </div>
     )
